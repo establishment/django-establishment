f5c415deee8c1215392c1056cb83c49d4800987d
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-04-05 15:25:58 +0300

    * cleanup chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,30 +1,24 @@
 from django.contrib import admin
 from django.core import urlresolvers
 
-from establishment.accounts.utils import get_user_search_fields
 from establishment.chat.models import *
 
 
 class MessageInstanceAdmin(admin.ModelAdmin):
     list_display = ('user', 'link_to_message_thread', 'content', "link_to_reaction_collection")
     list_filter = ('hidden',)
-    search_fields = []
+    search_fields = ('user', 'content',)
     raw_id_fields = ('message_thread',)
 
     def link_to_message_thread(self, obj):
-        link = urlresolvers.reverse("admin:chat_messagethread_change",
-                                    args=[obj.message_thread_id])  # model name has to be lowercase
+        link = urlresolvers.reverse("admin:chat_messagethread_change", args=[obj.message_thread_id]) #model name has to be lowercase
         return u'<a href="%s">%s</a>' % (link, "MessageThread-" + str(obj.message_thread_id))
     link_to_message_thread.allow_tags = True
-
+    
     def link_to_reaction_collection(self, obj):
-        link = urlresolvers.reverse("admin:accounts_userreactioncollection_change",
-                                    args=[obj.reaction_collection_id])  # model name has to be lowercase
+        link = urlresolvers.reverse("admin:accounts_userreactioncollection_change", args=[obj.reaction_collection_id]) #model name has to be lowercase
         return u'<a href="%s">%s</a>' % (link, "UserReactionCollection-" + str(obj.reaction_collection_id))
     link_to_reaction_collection.allow_tags = True
-
-    def get_search_fields(self, request):
-        return ["content"] + ["user__" + attr for attr in get_user_search_fields()]
 
 admin.site.register(MessageThread)
 admin.site.register(MessageInstance, MessageInstanceAdmin)
