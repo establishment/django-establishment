d19834b51fe743d0ce14a81f739990d5a05964ff
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-05-26 21:23:07 +0300

    * add support for interval worker

--- misc/threading_helper.py
+++ misc/threading_helper.py
@@ -1,13 +1,19 @@
 """
 Thread handler from fabric lib, to run background threads
 """
+import logging
 import threading
 import traceback
 import sys
 
+import time
+
+import datetime
+
 
 class ThreadHandler(object):
     def __init__(self, name, callable, *args, **kwargs):
+        self.name = name
         # Set up exception handling
         self.exception_info = None
 
@@ -26,22 +32,60 @@ class ThreadHandler(object):
         self.thread.setDaemon(is_daemon)
         self.thread.start()
 
-    def restart(self):
-        raise NotImplementedError("Still need to figure this out")
-
     def raise_if_needed(self):
         if self.exception:
             raise self.exception
 
+    def is_alive(self):
+        return self.thread.is_alive()
+
 
 class ThreadIntervalHandler(ThreadHandler):
-    def __init__(self, name, callable, interval, *args, **kwargs):
+    def __init__(self, name, worker, interval, *args, **kwargs):
         self.terminate = False
         self.interval = interval
+        self.start_at_interval_multiples = kwargs.pop("start_at_interval_multiples", False)
+        self.exception_throttle = kwargs.pop("exception_throttle", 3600)
+        self.worker = worker
+        self.signaler = kwargs.pop("signaler", False)
+        self.terminate_on_exception = kwargs.pop("terminate_on_exception", False)
+        self.logger = kwargs.get("logger") or logging.getLogger("django")
+        self.is_working = False
+
         super().__init__(name, self.background, *args, **kwargs)
 
+    def get_error_message(self, e):
+        return "Error in daemon thread " + str(self.name) + " " + str(e)
+
     def background(self, *args, **kwargs):
-        pass
+        last_exception_time = 0
+        last_start_time = 0.0
+        while not self.terminate and (not self.signaler or not self.signaler.terminate):
+            try:
+                last_start_time = time.time()
+                self.is_working = True
+                self.worker(*args, **kwargs)
+            except Exception as e:
+                current_time = time.time()
+                error_message = self.get_error_message(e)
+                if current_time - last_exception_time > self.exception_throttle:
+                    last_exception_time = current_time
+                    self.logger.exception(error_message)
+                else:
+                    self.logger.warn(error_message)
+                if self.terminate_on_exception:
+                    raise e
+            self.is_working = False
+            sleep_duration = self.interval
+            if self.start_at_interval_multiples:
+                dt = datetime.datetime.now()
+                microsec_since_midnight = ((dt.hour * 60 + dt.minute) * 60 + dt.second) * 1000000 + dt.microsecond
+                microsec_interval = int(self.interval * 1000000)
+                microsec_remaining = microsec_interval - int(microsec_since_midnight) % microsec_interval
+                sleep_duration = microsec_remaining / 1000000.0
+                if time.time() - last_start_time > self.interval:
+                    sleep_duration = 0
+            time.sleep(sleep_duration)
 
     def stop(self):
         self.terminate = True
--- services/service_daemon.py
+++ services/service_daemon.py
@@ -4,15 +4,24 @@ import django
 # The import encodings.idna is never user explicitly, we just need it for networking
 import encodings.idna
 
+import time
+
+import sys
+
+from establishment.misc.threading_helper import ThreadIntervalHandler
 from establishment.services.daemon import Daemon
 from establishment.services.status import ServiceStatus
 
+
 class ServiceDaemon(Daemon):
+    version = "1.0"
+
     def __init__(self, service_name, pidfile=None):
         if pidfile is None:
             pidfile = "." + service_name + "_daemon.pid"
 
         self.service_name = service_name
+        self.background_thread_handlers = []
         super().__init__(service_name, pidfile)
 
     def setup(self):
@@ -20,10 +29,35 @@ class ServiceDaemon(Daemon):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", module_name + ".settings")
         django.setup()
 
+    def log_initial_info(self):
+        self.logger.info(str(self.service_name) + " Daemon " + str(self.version))
+        self.logger.info("Operating system: " + os.name + " -- " + sys.platform)
+        self.logger.info("File system encoding: " + sys.getfilesystemencoding())
+
     def before_run(self):
         self.setup_logging()
-
+        self.log_initial_info()
         ServiceStatus.init(self.service_name)
+
+    def start_background_thread(self, worker, *args, **kwargs):
+        name = kwargs.pop("name", worker.__name__)
+        thread_handler = ThreadIntervalHandler(name, worker, *args,
+                                               signaler=self,
+                                               logger=self.logger,
+                                               **kwargs)
+        self.background_thread_handlers.append(thread_handler)
+
+    def wait_for_background_threads(self, wait_time=5):
+        while not self.terminate:
+            time.sleep(0.1)
+
+        termination_max_time = time.time() + wait_time
+
+        for thread_handler in self.background_thread_handlers:
+            while thread_handler.is_working and time.time() < termination_max_time:
+                time.sleep(0.1)
+            if thread_handler.is_working:
+                self.logger.error("Thread " + str(thread_handler.name) + " is still alive for too long, dying anyway")
 
     def start(self):
         super().start()
