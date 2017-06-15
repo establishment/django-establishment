96f6976e0847b90b0e4d4a8f09e4f54120017d45
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-06-15 13:45:10 +0300

    * add admin for global settings

--- baseconfig/admin.py
+++ baseconfig/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import PrivateGlobalSettings, PublicGlobalSettings, CommandInstance, CommandRun
 
-admin.site.register(PrivateGlobalSettings)
-admin.site.register(PublicGlobalSettings)
+
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    list_display = ("key", "value", "namespace")
+    list_filter = ("namespace",)
+    search_fields = ("key", "namespace")
+
+
+admin.site.register(PrivateGlobalSettings, GlobalSettingsAdmin)
+admin.site.register(PublicGlobalSettings, GlobalSettingsAdmin)
 admin.site.register(CommandInstance)
 admin.site.register(CommandRun)
