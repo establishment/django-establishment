b47fa3f7f0d6ca0555713de07ccf972328931cb5
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-08-25 21:27:45 +0300

    * move code to stem

--- webapp/js/GlobalStyleSheet.js
+++ webapp/js/GlobalStyleSheet.js
@@ -23,15 +23,4 @@ export class GlobalStyleSheet extends StyleSheet {
         textDecoration: "none",
         color: this.themeProperties.COLOR_LINK || "#337ab7"
     };
-
-    static instantiate() {
-        const styleSheet = this.getInstance();
-
-        for (const key of Object.keys(styleSheet.constructor.prototype)) {
-            // Just hit the getter to instantiate the style
-            styleSheet[key];
-        }
-    }
 }
-
-
