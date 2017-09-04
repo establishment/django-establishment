63201831f50964b5cba591d010d14194f6091e3a
darius98 <marian.darius98@gmail.com>
2017-09-04 18:42:33 +0300

    * something went wrong with previous commit

---
+++ content/js/PopupSpan.jsx
@@ -0,0 +1,42 @@
+import {UI} from "UI";
+
+import {BasePopup} from "./Popup";
+
+
+export class PopupSpan extends UI.Primitive("span") {
+    render() {
+        return [
+            <div ref="content">
+                {this.getGivenChildren()}
+            </div>
+        ];
+    }
+
+    onMount() {
+        this.content.addNodeListener("mouseover", () => {
+            let content = this.options.popupContent;
+            if (typeof content === "function") {
+                content = content();
+            }
+            this.popup = BasePopup.create(this.content, Object.assign({
+                target: this.content,
+                children: content,
+                transitionTime: 300,
+                titleFontSize: "10pt",
+                contentStyle: {
+                    padding: "0 0 0 5px",
+                    textAlign: "left"
+                },
+                style: {
+                    minWidth: "100px",
+                    maxWidth: "300px"
+                }
+            }, this.options.popupOptions || {}));
+        });
+        this.content.addNodeListener("mouseout", () => {
+            if (this.popup) {
+                this.popup.hide();
+            }
+        })
+    }
+}
