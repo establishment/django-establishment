2598d2d4bcc68bf60d84c6b3147c86fde0d37d8a
Marian <marian.darius98@gmail.com>
2018-03-13 16:06:19 +0200

    Add getFollowed method in Follower Store (app: social)

--- social/js/state/FollowerStore.js
+++ social/js/state/FollowerStore.js
@@ -1,11 +1,26 @@
 import {StoreObject, GenericObjectStore} from "state/Store";
 
+import {UserStore} from "UserStore";
+
 
 export class Follower extends StoreObject {
 
 }
 
 
-export const FollowerStore = new GenericObjectStore("social_follower", Follower, {
-    dependencies: ["user"]
-});
+class FollowerStoreClass extends GenericObjectStore {
+    constructor() {
+        super("social_follower", Follower, {
+            dependencies: ["user"]
+        });
+    }
+
+    getFollowed(userId) {
+        return FollowerStore.all().filter(follower => follower.userId === userId).map(
+            follower => UserStore.get(follower.targetId)
+        );
+    }
+}
+
+
+export const FollowerStore = new FollowerStoreClass();
