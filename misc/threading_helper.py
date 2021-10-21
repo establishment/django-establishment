077f413dbc31d3c666644716accccc3448f67a34
Mihai Ciucu <mihai.ciucu@gmail.com>
2021-10-21 17:53:32 +0300

    Always yield at least once in threading interval

--- misc/threading_helper.py
+++ misc/threading_helper.py
@@ -87,7 +87,7 @@ class ThreadIntervalHandler(ThreadHandler):
                 microsec_remaining = microsec_interval - int(microsec_since_midnight) % microsec_interval
                 sleep_duration = microsec_remaining / 1000000.0
                 if time.time() - last_start_time > self.interval:
-                    sleep_duration = 0
+                    sleep_duration = 0.001  # Do a minimal yield at least
             time.sleep(sleep_duration)
 
     def stop(self):
