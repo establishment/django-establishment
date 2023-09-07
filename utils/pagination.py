ca9dc7507e770ca1ade0b07fde8fbf5b1946c99c
Mihai Ciucu <mihai@blink.net>
2023-09-07 19:41:02 +0300

    Add pagination utils

---
+++ utils/pagination.py
@@ -0,0 +1,35 @@
+from typing import TypeVar, Callable, Optional
+
+from django.db.models import QuerySet, Model
+
+from establishment.utils.pydantic import PageQuery
+from establishment.utils.state import State, StateObjectList
+
+T = TypeVar("T", bound=Model)
+
+
+def paginate_entries(entries: QuerySet[T], page_query: PageQuery) -> QuerySet[T]:
+    start_index = (page_query.page - 1) * page_query.page_size
+    end_index = start_index + page_query.page_size
+    return entries[start_index: end_index]
+
+
+def paginate_to_state(objects: QuerySet[T],
+                      page_query: PageQuery,
+                      wrapper: Optional[Callable[[T], Optional[StateObjectList]]] = None,
+                      bulk_wrapper: Optional[Callable[[QuerySet[T]], Optional[StateObjectList]]] = None,
+                      extra: Optional[dict] = None) -> State:
+    paginated_objects = paginate_entries(objects, page_query)
+    state = State(extra=extra)
+    if wrapper is not None:
+        for obj in paginated_objects:
+            state.add(wrapper(obj))
+    elif bulk_wrapper is not None:
+        state.add(bulk_wrapper(paginated_objects))
+    else:
+        state.add(paginated_objects)
+    if page_query.include_count:
+        state.add_extra({
+            "count": objects.count(),
+        })
+    return state
