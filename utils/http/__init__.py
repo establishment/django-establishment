986a286ef0ce09564e06c3a4c18b6b1acbcacd23
Mihai Ciucu <mihai@blink.net>
2025-03-29 18:54:48 +0200

    Fix exporting permissions util

--- utils/http/__init__.py
+++ utils/http/__init__.py
@@ -1,8 +1,10 @@
 from .view_handler import *
 from .view_config import *
+from .permissions import *
 
 __all__ = [
     "BaseView",
+    "Permission",
     "ViewSet",
     "ViewConfig",
     "ViewMethod",
